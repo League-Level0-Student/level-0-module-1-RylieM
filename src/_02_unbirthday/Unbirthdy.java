8a2863659e861b29e56101bb58d4fdd2a2343d1d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2020-01-15 18:28:27 -0800

    1/15/20

---
+++ src/_02_unbirthday/Unbirthdy.java
@@ -0,0 +1,48 @@
+package _02_unbirthday;
+
+import java.nio.channels.ShutdownChannelGroupException;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthdy {
+  
+	public static void main(String[] args) {
+
+		String cat = "1/15/20";
+        String hat= 
+		JOptionPane. showInputDialog (null,"What is your birthday?");
+		
+		if ( hat.equals(cat)   )  {
+		JOptionPane.showMessageDialog(null,"Happy Birthday")	;
+		}
+		else {
+		JOptionPane.showMessageDialog(null, "Happy Unbirthday");
+		}
+		
+		
+		
+		
+		
+		
+		}
+	}
+	
+	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
